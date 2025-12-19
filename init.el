64b6fd2e867a14d443277e02f42952de31942091
p3rsonatic <109877225+p3rsonatic@users.noreply.github.com>
2025-12-19 15:41:40 -0300

    Update init.el

--- init.el
+++ init.el
@@ -108,3 +108,25 @@
   (setq org-src-tab-acts-natively t))    ;; Use language-specific TAB behavior
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-babel-load-languages
+   '((emacs-lisp . t) (python . t) (C . t) (latex . t) (haskell . t)))
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+ 
+ ;; Line numbering and soft wrap
+ 
+(global-visual-line-mode t) ;;soft-wrap
+
+(setq display-line-numbers-type 'relative) ;; relative line numbering 
+(global-display-line-numbers-mode 1)
+ 
